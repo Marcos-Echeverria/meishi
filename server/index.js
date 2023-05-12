273d3e9b989d294d270a03f171323f1c97d754b7
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-05-12 13:28:50 -0300

    routes

--- server/index.js
+++ server/index.js
@@ -4,13 +4,15 @@ const debug = require('debug')('app:main');
 const initDB = require('./src/database/index');
 const { Config } = require('./src/config/index');
 const MenuRouter = require('./src/routes/menu_routes');
+const cors = require('cors');
 
 //Enrutado
-app.use("/api", MenuRouter); 
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
+app.use("/api", MenuRouter);
 
 app.listen(Config.port, () => {
-    debug(`Servidor escuchando en el puerto ${Config.port}`)
+    debug(`Servidor escuchando en el puerto ${Config.port}`);
 });
 
-initDB()
+initDB();
