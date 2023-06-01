f017e222a7b117d58c5dca3026026b11ee8ace0a
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-06-01 17:49:45 -0300

    estructura basica del menu

--- client/src/components/pages/menu/menu.jsx
+++ client/src/components/pages/menu/menu.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react"
-import { todosMenu } from "../../functions/function"
+import { useEffect, useState } from "react";
+import { todosMenu } from "../../functions/function";
+import { Box, Typography } from "@mui/material";
 
 const Menu = () => {
   const [menus, setMenus] = useState(null);
@@ -8,17 +9,26 @@ const Menu = () => {
     todosMenu(setMenus);
   }, []);
   
+  const boxStyle = {
+    marginTop: "1vh",
+  };
+
   return (
     <>
+
+    <Box sx={boxStyle} >
       {menus != null ? (menus.map(menu => (
         <div key={menu.id}>
-          <a href="#">{menu.name}</a>
-          <a href="#">{menu.price}</a>
+          <Typography color="#4F5D3C">{menu.name}</Typography>
+          <Typography>{menu.description}</Typography>
+          <Typography>Precio: ${menu.price}</Typography>
         </div>
       ))
       ) : ('No hay menu')}
+    </Box>
+
     </>
-  )
-}
+  );
+};
 
 export default Menu
