826ac91ae097b41b17f8ed461934eea66b68ca96
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-06-15 13:16:51 -0300

    fondo y detalles del menu

--- client/src/components/pages/menu/menu.jsx
+++ client/src/components/pages/menu/menu.jsx
@@ -6,21 +6,30 @@ import "./Menu.css";
 const Menu = () => {
   const [menus, setMenus] = useState(null);
 
+  const imagenFondo = {
+    backgroundImage: "url('img-menu/fondo-menu2.jpg')",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    marginTop:"0px"
+  };
+
+
   useEffect(() => {
     todosMenu(setMenus);
   }, []);
 
   return (
     <>
-      <Typography variant="h4" component="div" className="menuHeader">
+      <Typography variant="h4" component="div" className="menuHeader" sx={imagenFondo} style={{marginTop:"1rem"}} >
         <Grid className="container-titulo" container spacing={2}>
           <Grid item xs={6} sm={6} md={6}>
-            <Typography className="menuHeaderText" variant="h3" component="div" style={{ fontFamily: "MiFuentePersonalizada" }}>
+            <Typography className="menuHeaderText" variant="h3" color="white" component="div" style={{ fontFamily: "MiFuentePersonalizada", textAlign:"-webkit-center" }}>
               Platos principales
             </Typography>
           </Grid>
           <Grid item xs={6} sm={6} md={6}>
-            <Typography className="menuHeaderText" variant="h3" component="div" style={{ fontFamily: "MiFuentePersonalizada" }}>
+            <Typography className="menuHeaderText" variant="h3" color="white" component="div" style={{ fontFamily: "MiFuentePersonalizada", textAlign:"-webkit-center" }}>
               Entradas
             </Typography>
           </Grid>
@@ -29,7 +38,7 @@ const Menu = () => {
 
       <Box className="menuContainer">
         {menus != null ? (
-          <Grid container spacing={2}>
+          <Grid container spacing={2} sx={imagenFondo} style={{textAlign:"-webkit-center"}}>
             <Grid item xs={6} sm={6} md={6}>
               {menus.map((menu) => {
                 if (menu.category === "Principal") {
