3004bc75161250fad5dd547a80b400cd0368e700
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-06-15 12:50:22 -0300

    Alineacion de columnas en menu

--- client/src/components/pages/menu/menu.jsx
+++ client/src/components/pages/menu/menu.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { todosMenu } from "../../functions/function";
 import { Box, Card, CardContent, Typography, Grid } from "@mui/material";
-import "./Menu.css"; 
+import "./Menu.css";
 
 const Menu = () => {
   const [menus, setMenus] = useState(null);
@@ -10,64 +10,75 @@ const Menu = () => {
     todosMenu(setMenus);
   }, []);
 
-  
   return (
     <>
-    <Typography variant="h4" component="div" className="menuHeader">
-      <Grid container columns={{ xs: 4, sm: 12, md: 12 }}>
-        <Grid className="menuHeaderText" item xs={2} sm={4} md={4} style={{ fontFamily: "MiFuentePersonalizada"}}>
-          Menú principal
+      <Typography variant="h4" component="div" className="menuHeader">
+        <Grid className="container-titulo" container spacing={2}>
+          <Grid item xs={6} sm={6} md={6}>
+            <Typography className="menuHeaderText" variant="h3" component="div" style={{ fontFamily: "MiFuentePersonalizada" }}>
+              Platos principales
+            </Typography>
+          </Grid>
+          <Grid item xs={6} sm={6} md={6}>
+            <Typography className="menuHeaderText" variant="h3" component="div" style={{ fontFamily: "MiFuentePersonalizada" }}>
+              Entradas
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid className="menuHeaderText" item xs={2} sm={4} md={4} style={{ fontFamily: "MiFuentePersonalizada"}}>
-          Entradas
-        </Grid>
-      </Grid>
-    </Typography>
+      </Typography>
 
-      <Box className="menuContainer" display="grid" gridTemplateColumns="repeat(2, 1fr)">
+      <Box className="menuContainer">
         {menus != null ? (
-          menus.map((menu) => {
-            if (menu.category === "Principal") {
-              return (
-                <>
-                <Box className="principal">
-                <Card key={menu._id} className="menuCard">
-                  <CardContent>
-                    <Typography className="menuTitle" variant="h6" component="div">
-                      {menu.name}
-                    </Typography>
-                    <Typography className="menuDescription" variant="body2" color="text.secondary">
-                      {menu.description}
-                    </Typography>
-                    <Typography className="menuPrice" variant="body1">
-                      Precio: ${menu.price}
-                    </Typography>
-                  </CardContent>
-                </Card>
-                </Box>
-                </>
-              );
-            } else if ( menu.category === "Entrada"){
-              return (
-              <> 
-              <Box className="Entradas">
-              <Card key={menu._id} className="menuCard">
-                <CardContent>
-                  <Typography className="menuTitle" variant="h6" component="div">
-                    {menu.name}
-                  </Typography>
-                  <Typography className="menuDescription" variant="body2" color="text.secondary">
-                    {menu.description}
-                  </Typography>
-                  <Typography className="menuPrice" variant="body1">
-                    Precio: ${menu.price}
-                  </Typography>
-                </CardContent>
-              </Card>
-              </Box>
-              </>
-)}
-          })
+          <Grid container spacing={2}>
+            <Grid item xs={6} sm={6} md={6}>
+              {menus.map((menu) => {
+                if (menu.category === "Principal") {
+                  return (
+                    <Box key={menu._id} className="principal">
+                      <Card className="menuCard">
+                        <CardContent>
+                          <Typography className="menuTitle" variant="h6" component="div">
+                            {menu.name}
+                          </Typography>
+                          <Typography className="menuDescription" variant="body2" color="text.secondary">
+                            {menu.description}
+                          </Typography>
+                          <Typography className="menuPrice" variant="body1">
+                            Precio: ${menu.price}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Box>
+                  );
+                }
+                return null;
+              })}
+            </Grid>
+            <Grid item xs={6} sm={6} md={6}>
+              {menus.map((menu) => {
+                if (menu.category === "Entrada") {
+                  return (
+                    <Box key={menu._id} className="principal">
+                      <Card className="menuCard">
+                        <CardContent>
+                          <Typography className="menuTitle" variant="h6" component="div">
+                            {menu.name}
+                          </Typography>
+                          <Typography className="menuDescription" variant="body2" color="text.secondary">
+                            {menu.description}
+                          </Typography>
+                          <Typography className="menuPrice" variant="body1">
+                            Precio: ${menu.price}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Box>
+                  );
+                }
+                return null;
+              })}
+            </Grid>
+          </Grid>
         ) : (
           <Typography variant="body1">
             El menú está en reparación. Por favor, aguarda unos minutos.
